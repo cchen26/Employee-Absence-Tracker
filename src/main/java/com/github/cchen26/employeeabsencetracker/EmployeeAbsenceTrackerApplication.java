3b5e7051b8cae5b52155a97dc4bd2ea172a92562
Chao Chen <chaochen234@gmail.com>
2023-12-15 17:13:01 -0500

    Initial commit

---
+++ src/main/java/com/github/cchen26/employeeabsencetracker/EmployeeAbsenceTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.github.cchen26.employeeabsencetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeAbsenceTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeAbsenceTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/cchen26/employeeabsencetracker/EmployeeAbsenceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.cchen26.employeeabsencetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeAbsenceTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
